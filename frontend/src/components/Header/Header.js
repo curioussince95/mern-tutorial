48c4fb54af6ca86d164196b68d3991f3fd779371
Gamaliel Lumanta <lumantagamaliel.yns@gmail.com>
2022-04-02 00:26:06 +0800

    Added navbar landing page and footer to frontend

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import { Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar bg="primary" expand="lg" variant="dark">
+            <Container>
+                <Navbar.Brand href="/">Notezipper</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="m-auto">
+                        <Form className="d-flex">
+                            <FormControl type="text" placeholder='Search' className="mr-sm-2" />
+                        </Form>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#home">My Notes</Nav.Link>
+                        <NavDropdown title="Gamaliel" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
